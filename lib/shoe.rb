550c0d7da0c152302f4c12743d3729201a65d1e4
Victor Mayi <victormay.i.797@gmail.com>
2022-10-25 15:00:21 +0300

    commit tree 1724ec80d59234b0832f81e54b7da973be1d25e4

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
-# Make your shoe class here!
+
+class Shoe
+    attr_reader :brand
+    attr_accessor :color, :size, :material, :condition
+
+    def initialize(brand, color = "Black and white", size = 43, material = "Leather", condition = "Worn")
+        @brand = brand
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
